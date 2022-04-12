20d994b0b1e0dd7809dc75545dc59bc1bebfe870
mjoh223 <matthew.johnson@ucsf.edu>
2022-04-12 13:15:40 -0700

    parallel feature

--- fasta2genbank.py
+++ fasta2genbank.py
@@ -10,48 +10,55 @@ from Bio.SeqFeature import SeqFeature, FeatureLocation
 import subprocess
 import random
 import string
-
+import glob
+import sys
 def randomString(stringLength=8):
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(stringLength))
 
 def makedir(working_directory):
-    f  =  os.path.join(working_directory, randomString(4))
+    f  =  os.path.join(working_directory, randomString(6))
     os.mkdir(f)
     directories = ['tmp']
     for basename in directories:
         os.mkdir( os.path.join(f, basename) )
     return f
 
-def build_icm(tmp_dir):
-    filename = os.path.join(tmp_dir, 'user_fasta.fna')
+def build_icm(filename, tmp_dir):
+    print('building ICM')
+    #filename = os.path.join(tmp_dir, 'user_fasta.fna')
     output_dir = os.path.join(tmp_dir, 'genome.icm')
     subprocess.run(['/bin/build-icm ' + output_dir + ' < ' + filename], shell=True)    
 
-def glimmer3(tmp_dir):
-    filename_fna = os.path.join(tmp_dir, 'user_fasta.fna')
+def glimmer3(filename, tmp_dir):
+    print('running glimmer3')
+    #filename_fna = os.path.join(tmp_dir, 'user_fasta.fna')
     genome_icm = os.path.join(tmp_dir, 'genome.icm')
-    subprocess.run(['/bin/glimmer3 ' + filename_fna + ' ' + genome_icm + ' ' + os.path.join(tmp_dir,'tmp','genome')], shell=True)
+    subprocess.run(['/bin/glimmer3 -l --gc_percent 67.2 ' + filename + ' ' + genome_icm + ' ' + os.path.join(tmp_dir,'tmp','genome')], shell=True)
 
-def extract(tmp_dir):
-    filename_fna = os.path.join(tmp_dir, 'user_fasta.fna')
-    subprocess.run(['/bin/extract ' + filename_fna + ' ' + os.path.join(tmp_dir, 'tmp', 'genome.predict') + ' > ' + os.path.join(tmp_dir, 'tmp', 'genome.fna')], shell=True)
+def extract(filename, tmp_dir):
+    print('extracting')
+    #filename_fna = os.path.join(tmp_dir, 'user_fasta.fna')
+    subprocess.run(['/bin/extract ' + filename + ' ' + os.path.join(tmp_dir, 'tmp', 'genome.predict') + ' > ' + os.path.join(tmp_dir, 'tmp', 'genome.fna')], shell=True)
 
-def makeGenbank(tmp_dir, information):
+def makeGenbank(filename_fna, tmp_dir, information):
+    print('making genbank')
     #fn = os.path.basename(os.path.normpath(filename_fna))
     #name = fn.split('.')[0]
     name = 'output.gbk'
-    filename_fna = os.path.join(tmp_dir, 'user_fasta.fna')
+    #filename_fna = os.path.join(tmp_dir, 'user_fasta.fna')
     with open(filename_fna) as handle:
         for record in SeqIO.parse(handle, "fasta"):
             fna = record.seq
             print(len(fna))
     #create seqRecord
-    record_to_write = SeqRecord(Seq(str(fna), generic_dna), id="Personal", description='Personal', name=name, annotations=information)
+    record_to_write = SeqRecord(Seq(str(fna)), id="Personal", description='Personal', name=name, annotations=information)
     #create features
     filename_predict = os.path.join(tmp_dir, 'tmp', 'genome.fna')
     with open(filename_predict) as handle:
         for record in SeqIO.parse(handle, "fasta"):
+            print(record.seq)
+            print(record.seq.translate())
             qualifiers = {
             'locus_tag': record.id,
             'product': 'unknown',
@@ -62,6 +69,8 @@ def makeGenbank(tmp_dir, information):
             start, end = record.description.split()[1:3]
             start = int(start)
             end = int(end)
+            print(record.description)
+            print([start,end])
             strand = 1
             if end < start:
                 start, end = end, start
@@ -79,7 +88,7 @@ def makeGenbank(tmp_dir, information):
     handle.close()
     #return os.path.join(tmp_dir, 'tmp', output_filename)
 
-if __name__ == '__main__':
+def fasta2Gembase(files):
     information = {
         'molecule_type':'DNA',
         'topology': 'linear',
@@ -96,11 +105,13 @@ if __name__ == '__main__':
           'Pseudomonadaceae',
           'Pseudomonas'],
         }
-
-    filename_fna = '/home/jbd_apps/fasta_to_genbank/KMV_phages/gdFB465.fasta'
-    tmp_dir = makedir('/home/jbd_apps/fasta_to_genbank')
+    #for filename_fna in files:
+    filename_fna = sys.argv[1]
+    tmp_dir = makedir('/hdd-roo/DHS/meta_gbk/')
     build_icm(filename_fna, tmp_dir)
     glimmer3(filename_fna, tmp_dir)
     extract(filename_fna, tmp_dir)
-    output_filename = makeGenbank(tmp_dir, information, filename_fna)
-    print('output at {}'.format(output_filename)) 
+    output_filename = makeGenbank(filename_fna, tmp_dir, information)
+    print('output at {}'.format(output_filename))
+
+fasta2Gembase(glob.glob('/hdd-roo/DHS/meta_fna/*fna'))
--- script.py
+++ script.py
@@ -1,14 +1,36 @@
 from Bio import SeqIO
 import glob
 import pandas as pd
+import os
 from phamlite import drawOrf, graphing
+from multiprocessing import Pool
+def parallelcreategembasefromDHS(DHS_record_features, assembly, contig):
+    f = open(os.path.join('/hdd-roo/DHS/parallel/', contig), "w")
+    for feature in DHS_record_features:
+        if feature.type == 'CDS' and 'protein_id' in feature.qualifiers:
+            f.write('>{}q{}_{}\n'.format(
+                        assembly,
+                        contig,
+                        feature.qualifiers['protein_id'][0].split('.')[0].replace('_','')
+                        ))
+            f.write('{}\n'.format(feature.qualifiers['translation'][0]))
+
+def createGembasefromDHS(DHS_record_features, assembly, contig):
+    for feature in DHS_record_features:
+        if feature.type == 'CDS' and 'protein_id' in feature.qualifiers:
+            print('>{}q{}_{}'.format(
+                        assembly,
+                        contig,
+                        feature.qualifiers['protein_id'][0].split('.')[0].replace('_','')
+                        ))
+            print(feature.qualifiers['translation'][0])
 
 def createGembase(gb_files):
     for gb_file in gb_files:
         for gb_record in SeqIO.parse(open(gb_file,"r"), "genbank") :
             assembly = gb_record.dbxrefs[0].split(':')[1].replace('_','').replace('.','')
             for feature in gb_record.features:
-                if feature.type == 'CDS':
+                if feature.type == 'CDS' and 'protein_id' in feature.qualifiers:
                     print('>{}q{}_{}'.format(
                         assembly,
                         gb_record.name.replace('_','').replace('.',''),
@@ -24,10 +46,14 @@ def grabDHS(record, boundary_set):
     l_coord = [x[0] for x in enumerate(record.features) if x[1].qualifiers.get('locus_tag', [''])[0] in boundary_set[0]]
     r_coord = [x[0] for x in enumerate(record.features) if x[1].qualifiers.get('locus_tag', [''])[0] in boundary_set[1]]
     if len(l_coord) == 2 and len(r_coord) == 2:
+        assembly = record.dbxrefs[0].split(':')[1].replace('_','').replace('.','')
+        contig = record.name.replace('_','').replace('.','')
         coords = sorted([l_coord,r_coord])
         DHS = record.features[coords[0][0]:coords[1][1]]
-        graph(DHS)
-        print(lol)
+        #print(DHS)
+        parallelcreategembasefromDHS(DHS, assembly, contig)
+        #graph(DHS)
+        #print(lol)
 def readGenbanks(filename, boundary_set):
     gb_files = glob.glob(filename)
     for gb_file in gb_files:
@@ -40,7 +66,18 @@ def readMMseqOut(filenames):
     df1 = set(df1.iloc[:,1])
     return [df0, df1]
 
-createGembase(glob.glob('/hdd-roo/DHS/gbk/*gbk'))
+def runner(gb_file):
+    [grabDHS(gb_record, boundary_set) for gb_record in SeqIO.parse(open(gb_file,"r"), "genbank")]
 
-#boundary_set = readMMseqOut(['/hdd-roo/DHS/lb_results', '/hdd-roo/DHS/rb_results'])
+def parallel(genbanks):
+    #for genbank in genbanks:
+    p = Pool(127)
+    p.map(runner, genbanks)
+
+boundary_set = readMMseqOut(['/hdd-roo/DHS/lb_results', '/hdd-roo/DHS/rb_results'])
+genbanks = glob.glob('/hdd-roo/DHS/gbk/*gbk')
+parallel(genbanks)
+#createGembase(glob.glob('/hdd-roo/DHS/gbk/*gbk'))
 #readGenbanks('/hdd-roo/DHS/gbk/*gbk', boundary_set)
+
+
